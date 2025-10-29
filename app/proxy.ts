1f3341664959517d5993045062c482e35789c97e
Alanna Matundan <rosealanna18@gmail.com>
2025-10-29 18:43:36 -0500

    fixed proxy.ts

--- app/proxy.ts
+++ app/proxy.ts
@@ -13,23 +13,23 @@ export async function middleware(req: NextRequest) {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get: (name: string) => {
-          return req.cookies.get(name)?.value
+        get(name: string) {
+          return req.cookies.get(name)?.value;
         },
-        set: (name: string, value: string, options: CookieOptions) => {
+        set(name: string, value: string, options: CookieOptions) {
           res.cookies.set({
             name,
             value,
             ...options,
-          })
+          });
         },
-        remove: (name: string, options: CookieOptions) => {
+        remove(name: string, options: CookieOptions) {
           res.cookies.set({
             name,
             value: '',
             ...options,
             maxAge: 0,
-          })
+          });
         },
       },
     }
